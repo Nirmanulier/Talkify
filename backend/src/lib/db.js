0177cfa3f210d091cec59b5b54a588607f482a4c
Nirmanulier <nirman11940@gmail.com>
2026-06-22 14:34:12 +0530

    should be final commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(ENV.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {
     console.error("Error connection to MONGODB:", error);
